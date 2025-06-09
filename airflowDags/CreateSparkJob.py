52d8125d3ea6b0ec9eec48174a385af8b5c5ebc2
LeartIGashi <lattgashi@gmail.com>
2025-06-10 00:14:09 +0200

    Update CreateSparkJob.py
    New Last Straw

--- airflowDags/CreateSparkJob.py
+++ airflowDags/CreateSparkJob.py
@@ -1,11 +1,9 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
-from kubernetes import client, config
-from kubernetes.client.rest import ApiException
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime
-import yaml
-import logging
+import tempfile
+import os
 
 spark_app_yaml = """
 apiVersion: spark.stackable.tech/v1alpha1
@@ -37,54 +35,12 @@ spec:
         claimName: shared-spark-pvc
 """
 
-def submit_spark_job():
-    # Kubernetes Config laden (im Cluster oder local)
-    try:
-        config.load_incluster_config()
-    except config.ConfigException:
-        config.load_kube_config()
-
-    # YAML in Python dict parsen
-    spark_app = yaml.safe_load(spark_app_yaml)
-
-    # CustomObjects API benutzen
-    api = client.CustomObjectsApi()
-
-    group = "spark.stackable.tech"
-    version = "v1alpha1"
-    namespace = "default"
-    plural = "sparkapplications"
-
-    try:
-        # SparkApplication erstellen
-        api.create_namespaced_custom_object(
-            group=group,
-            version=version,
-            namespace=namespace,
-            plural=plural,
-            body=spark_app
-        )
-        logging.info("SparkApplication created successfully")
-    except ApiException as e:
-        if e.status == 409:
-            logging.info("SparkApplication already exists, updating it")
-            api.replace_namespaced_custom_object(
-                group=group,
-                version=version,
-                namespace=namespace,
-                plural=plural,
-                name=spark_app["metadata"]["name"],
-                body=spark_app
-            )
-        else:
-            logging.error(f"Failed to create SparkApplication: {e}")
-            raise
-
 with DAG(
-    "clone_and_run_spark_inline_yaml",
+    "clone_and_run_spark_k8s_pod",
     start_date=datetime(2023, 1, 1),
     schedule_interval=None,
-    catchup=False
+    catchup=False,
+    tags=["spark", "kubernetes"],
 ) as dag:
 
     clone_repo = BashOperator(
@@ -97,9 +53,44 @@ with DAG(
         """
     )
 
-    run_spark_job = PythonOperator(
-        task_id='submit_spark_app',
-        python_callable=submit_spark_job
+    # Write spark_app_yaml to a temporary file on the Airflow worker, so we can mount it to the pod
+    def write_yaml_to_tmp():
+        tmp_dir = "/tmp/airflow_spark_app"
+        os.makedirs(tmp_dir, exist_ok=True)
+        path = os.path.join(tmp_dir, "sparkapplication.yaml")
+        with open(path, "w") as f:
+            f.write(spark_app_yaml)
+        return path
+
+    # Using a short PythonOperator just to write the YAML file, or you can do this manually beforehand
+    from airflow.operators.python import PythonOperator
+    write_yaml = PythonOperator(
+        task_id="write_spark_yaml",
+        python_callable=write_yaml_to_tmp,
     )
 
-    clone_repo >> run_spark_job
+    submit_spark_app = KubernetesPodOperator(
+        namespace='default',
+        image="bitnami/kubectl:latest",
+        cmds=["kubectl", "apply", "-f", "/sparkapplication.yaml"],
+        name="submit-spark-application",
+        task_id="submit_spark_application",
+        get_logs=True,
+        is_delete_operator_pod=True,
+        volumes=[{
+            "name": "spark-yaml-volume",
+            "hostPath": {
+                "path": "/tmp/airflow_spark_app",
+                "type": "Directory"
+            }
+        }],
+        volume_mounts=[{
+            "name": "spark-yaml-volume",
+            "mountPath": "/",
+            "readOnly": True
+        }],
+        # optionally specify the service account name if you have one with proper RBAC permissions
+        # service_account_name="airflow-k8s-sa",
+    )
+
+    clone_repo >> write_yaml >> submit_spark_app
