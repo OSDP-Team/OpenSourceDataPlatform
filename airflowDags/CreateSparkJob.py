d98f2f7343bb0ed6ab1dffa53507135092d15694
LeartIGashi <lattgashi@gmail.com>
2025-06-10 22:41:23 +0200

    Update CreateSparkJob.py
    changed pvc mounted on spark

--- airflowDags/CreateSparkJob.py
+++ airflowDags/CreateSparkJob.py
@@ -35,7 +35,7 @@ spark_app = {
             {
                 "name": "shared-volume",
                 "persistentVolumeClaim": {
-                    "claimName": "spark-shared-pvc"
+                    "claimName": "shared-spark-pvc"
                 }
             }
         ],
