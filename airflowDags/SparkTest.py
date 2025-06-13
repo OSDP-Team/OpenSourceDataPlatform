b00981776216ec51b4b14b4df59cb50bf81e1186
LeartIGashi <lattgashi@gmail.com>
2025-06-13 23:26:32 +0200

    Update SparkTest.py
    Brauch einen test

--- airflowDags/SparkTest.py
+++ airflowDags/SparkTest.py
@@ -1,10 +1,1 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("MinimalTestJob").getOrCreate()
-
-# Create a simple DataFrame
-data = spark.range(0, 100)
-count = data.count()
-
-print(f"Total count is: {count}")
-spark.stop()
+print("Leart du geile Sau dein RepoImage funktioniert")
