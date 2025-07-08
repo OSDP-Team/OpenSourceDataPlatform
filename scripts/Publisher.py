e82e4060e3c383e926dca93b45b4542ee95ee778
LeartIGashi <lattgashi@gmail.com>
2025-07-09 00:21:58 +0200

    Improved Readability & Logging. Added Filenames

--- scripts/Publisher.py
+++ scripts/Publisher.py
@@ -32,13 +32,24 @@ def generate_spark_dataframe(spark, start_time: datetime, num_rows: int, modul: 
         pval = round(np.random.normal(0.605, 0.002), 6)
 
         row = (
-            50, 0, 0,
-            current_time.strftime("%d.%m.%Y %H:%M"),
-            min_val, (current_time - timedelta(minutes=15)).strftime("%d.%m.%Y %H:%M"),
-            max_val, (current_time - timedelta(minutes=15)).strftime("%d.%m.%Y %H:%M"),
-            avg_val, sum_val, pval,
-            1, "01.01.0001 00:00:00", "",  # OFFSET, VERSION, TEXT
-            modul, intervall, 1, 0         # MESS_ID, COMP_LEVEL
+            50, #STATE_VAL
+            0,  #STATE_ACQ
+            0,  #STATE_COR
+            current_time.strftime("%d.%m.%Y %H:%M"), #ENTRYDATE
+            min_val, #MIN
+            (current_time - timedelta(minutes=15)).strftime("%d.%m.%Y %H:%M"), #MINDATE
+            max_val, #MAX
+            (current_time - timedelta(minutes=15)).strftime("%d.%m.%Y %H:%M"), #MAXDATE
+            avg_val, #AVG
+            sum_val, #SUM
+            pval, #PVALUE
+            1, #OFFSET
+            "01.01.0001 00:00:00", #VERSION
+            "", #TEXT
+            modul, #modul
+            intervall, #intervall
+            1, #MESS_ID
+            0  #COMP_LEVEL
         )
         rows.append(row)
         current_time += timedelta(minutes=15)
@@ -68,44 +79,47 @@ def generate_spark_dataframe(spark, start_time: datetime, num_rows: int, modul: 
 
 def main():
     # Beispiel-Dateiname (realistisch in der Pipeline)
-    filename = "a_Strom_GOM_M2_15min_kWh_20220101000000_20230101000000"
-
-    modul, intervall = extract_metadata_from_filename(filename)
+    filenames = ["a_Strom_GOM_M1_15min_kWh_20220101000000_20230101000000","a_Strom_GOM_M2_15min_kWh_20220101000000_20230101000000", "a_Strom_GOM_M4_15min_kWh_20220101000000_20230101000000"]
+  
 
     minio_user = os.getenv("MINIO_ACCESS_KEY")
     minio_pwd = os.getenv("MINIO_SECRET_KEY")
-
     if not minio_user or not minio_pwd:
-        raise ValueError("MINIO_ACCESS_KEY oder MINIO_SECRET_KEY nicht gesetzt")
+     raise ValueError("MINIO_ACCESS_KEY oder MINIO_SECRET_KEY nicht gesetzt")
 
     spark = (
         SparkSession.builder
-            .appName("simulate-publisher-spark")
-            .master("local[*]")
-            .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.688")
-            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-            .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000")
-            .config("spark.hadoop.fs.s3a.path.style.access", "true")
-            .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
-            .config("spark.hadoop.fs.s3a.access.key", minio_user)
-            .config("spark.hadoop.fs.s3a.secret.key", minio_pwd)
-            .getOrCreate()
-    )
+         .appName("simulate-publisher-spark")
+         .master("local[*]")
+         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.688")
+         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+         .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000")
+         .config("spark.hadoop.fs.s3a.path.style.access", "true")
+         .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
+         .config("spark.hadoop.fs.s3a.access.key", minio_user)
+         .config("spark.hadoop.fs.s3a.secret.key", minio_pwd)
+         .getOrCreate()
+        )
 
     start_time = datetime.strptime("01.01.2022 00:00", "%d.%m.%Y %H:%M")
-    df = generate_spark_dataframe(spark, start_time, num_rows=96, modul=modul, intervall=intervall)
+    for filename in filenames:
+        modul, intervall = extract_metadata_from_filename(filename)
+        try:
+            df = generate_spark_dataframe(spark, start_time, num_rows=96, modul=modul, intervall=intervall)
 
-    output_path = f"s3a://producer/{filename}.csv"
+            output_path = f"s3a://producer/{filename}.csv"
 
-    df.write \
-        .mode("overwrite") \
-        .option("header", True) \
-        .option("sep", ";") \
-        .csv(output_path)
+            df.write \
+                .mode("overwrite") \
+                .option("header", True) \
+                .option("sep", ";") \
+                .csv(output_path)
 
-    print(f"Synthetische Daten erfolgreich geschrieben nach: {output_path}")
+            print(f"Synthetische Daten erfolgreich geschrieben nach: {output_path}")
+        except Exception as e:
+            print(f"Ein Fehler ist beim Schreiben der Daten aufgetreten: {e}")
 
-    spark.stop()
+        spark.stop()
 
 if __name__ == "__main__":
     import numpy as np
